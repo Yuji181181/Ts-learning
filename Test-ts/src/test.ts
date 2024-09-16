63795e9327818441d5978797a53649f9244c54e9
Yuji181181 <s1f102402552@iniad.org>
2024-09-16 16:15:17 +0900

    変数

--- Test-ts/src/test.ts
+++ Test-ts/src/test.ts
@@ -1,3 +1,10 @@
-export const test = () => {
-    console.log("test")
-}
+/**
+ * 変数に型をつける
+ */
+const test1:string = "aaa"
+const test2:number = 123
+const test3:boolean = true
+
+/**
+ * 配列に型をつける
+ */
