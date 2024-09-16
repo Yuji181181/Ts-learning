f53bf52e7a4a4c8a6fb39929b32365972c771030
Yuji181181 <s1f102402552@iniad.org>
2024-09-16 16:39:51 +0900

    修正

--- Test-ts/src/test.ts
+++ Test-ts/src/test.ts
@@ -13,7 +13,7 @@ const test5:(string | number)[] = ["aaa",123]
 const test6:[string,number] = ["aaa",123]
 
 /**
- * 関数に型をつける
+ * アロー関数に型をつける
  */
 //const 関数名 = (引数:型):戻り値 => {}
 const test7 = (x1:string, y1:number):void => {   //返り値がない場合はvoidをつける
