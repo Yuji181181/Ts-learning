104e66206d2324493ea270b449e0702db15840c9
Yuji181181 <s1f102402552@iniad.org>
2024-09-16 16:22:43 +0900

    アロー関数

--- Test-ts/src/test.ts
+++ Test-ts/src/test.ts
@@ -12,3 +12,10 @@ const test4:string[] = ["aaa","bbb"]
 const test5:(string | number)[] = ["aaa",123]
 const test6:[string,number] = ["aaa",123]
 
+/**
+ * 関数に型をつける
+ */
+//const 関数名 = (引数:型):戻り値 => {}
+const test7 = (x1:string, y1:number):void => {   //返り値がない場合はvoidをつける
+    console.log()
+}
